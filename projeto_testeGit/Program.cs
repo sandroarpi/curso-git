d5778f3c817434634d8aa2ae5d7865eef9afab47
Sandro Arpi <sandroarpi@yahoo.com.br>
2018-10-06 02:52:45 -0300

    Projeto iniciado

---
+++ projeto_testeGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_testeGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
