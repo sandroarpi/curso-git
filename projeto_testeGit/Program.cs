c0523e918398d5a5243a727893d1c830d55e681d
Sandro Arpi <sandroarpi@yahoo.com.br>
2018-10-08 00:51:35 -0300

    Pergunta como esta

--- projeto_testeGit/Program.cs
+++ projeto_testeGit/Program.cs
@@ -12,6 +12,7 @@ namespace projeto_testeGit
         {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está?");
             Console.ReadLine();
         }
     }
